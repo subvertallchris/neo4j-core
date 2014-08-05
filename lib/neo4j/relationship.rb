014f5e65cdf5c07be0dd7a418800bade0af428c4
Chris Grigg <chris@subvertallmedia.com>
2014-08-05 16:48:00 -0400

    allows relationship wrapping

--- lib/neo4j/relationship.rb
+++ lib/neo4j/relationship.rb
@@ -152,9 +152,13 @@ module Neo4j
       end
 
       def load(neo_id, session = Neo4j::Session.current)
-        session.load_relationship(neo_id)
+        rel = _load(neo_id, session)
+        rel && rel.wrapper
       end
 
+      def _load(neo_id, session = Neo4j::Session.current)
+        session.load_relationship(neo_id)
+      end
     end
   end
 end
